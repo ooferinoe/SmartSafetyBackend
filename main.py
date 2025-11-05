0d57d6422327331151e95d4e426033f596d0012d
Step in <a.stephenlance@gmail.com>
2025-11-05 20:01:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,6 @@ async def detect_ppe_violation(file: UploadFile = File(...)):
     return results
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
